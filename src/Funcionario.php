b3397b29186d8bd38c1f0f3d3a69b09d5a4523a5
Victor Costa <87447640+victorcosta21@users.noreply.github.com>
2022-06-25 12:13:40 -0300

    Adicionado pessoa.php e realizado algumas melhorias gerais no codigo

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -1,29 +1,17 @@
 <?php
 
-class Funcionario
+class Funcionario extends Pessoa
 {
-    private string $nome;
-    private string $cpf;
     private string $cargo;
 
-    public function __construct(string $nome, string $cpf, string $cargo)
+    public function __construct(string $nome, CPF $cpf, string $cargo)
     {
         $this->nome = $nome;
         $this->cpf = $cpf;
         $this->cargo = $cargo;
     }
 
-    public function recuperaNome() : string
-    {
-        return $this->nome;
-    }
-
-    public function recuperaCpf() : string
-    {
-        return $this->cpf;
-    }
-
-    public function recuperaCargp() : string
+    public function recuperaCargo() : string
     {
         return $this->cargo;
     }
--- src/Titular.php
+++ src/Titular.php
@@ -1,9 +1,7 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private CPF $cpf;
-    private string $nome;
     private Endereco $endereco;
 
     public function __construct(CPF $cpf, string $nome, Endereco $endereco)
@@ -12,24 +10,6 @@ class Titular
         $this->validaNomeTitular($nome);
         $this->nome = $nome;
         $this->endereco = $endereco;
-    }
-
-    public function recuperaCpf(): string
-    {
-        return $this->cpf->recuperaNumero();
-    }
-
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
     }
 
     public function recuperaEndereco() : Endereco
