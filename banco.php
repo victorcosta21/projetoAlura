c0b3d64f070748201d2b38b7a19590cda1d9d092
Victor Costa <87447640+victorcosta21@users.noreply.github.com>
2022-07-15 10:30:03 -0300

    modified:   banco.php 	renamed:    src/CPF.php -> src/Modelo/CPF.php 	renamed:    src/Conta.php -> src/Modelo/Conta/Conta.php 	renamed:    src/Titular.php -> src/Modelo/Conta/Titular.php 	renamed:    src/Endereco.php -> src/Modelo/Endereco.php 	renamed:    src/Funcionario.php -> src/Modelo/Funcionario.php 	renamed:    src/Pessoa.php -> src/Modelo/Pessoa.php

--- banco.php
+++ banco.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once 'src/Conta.php';
-require_once 'src/Endereco.php';
-require_once 'src/Titular.php';
-require_once 'src/CPF.php';
+require_once 'src/Modelo/Conta/Conta.php';
+require_once 'src/Modelo/Endereco.php';
+require_once 'src/Modelo/Conta/Titular.php';
+require_once 'src/Modelo/CPF.php';
 
 
 $endereco = new Endereco('cidade', 'bairro', 'rua', '13');
--- src/Modelo/Pessoa.php
+++ src/Modelo/Pessoa.php
@@ -1,33 +1,35 @@
 <?php
 
+namespace Modelo;
+
 class Pessoa
 {
-   protected $nome;
-   private $cpf;
+    protected string $nome;
+    private CPF $cpf;
+
 
     public function __construct(string $nome, CPF $cpf)
     {
-        $this->validaNomeTitular($nome);
+        $this->validaNome($nome);
         $this->nome = $nome;
-        $this->cpf = cpf;
+        $this->cpf = $cpf;
     }
 
-    public function recuperaNome() : string
+    public function recuperaNome(): string
     {
         return $this->nome;
     }
 
-    public function recuperaCpf() : string
+    public function recuperaCpf(): string
     {
         return $this->cpf->recuperaNumero();
     }
 
-    protected function validaNomeTitular(string $nomeTitular)
+    protected function validaNome(string $nome)
     {
-        if (strlen($nomeTitular) < 5) {
+        if (strlen($nome) < 5) {
             echo "Nome precisa ter pelo menos 5 caracteres";
             exit();
         }
     }
-
 }
